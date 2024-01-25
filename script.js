9075165a8bd0e7d971bb3a3d5ab04d95a2ff48e3
afgolik <64197556+afgolik@users.noreply.github.com>
2024-01-25 22:47:24 +0300

    done

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-const body = document.querySelector('body');
-const preloader = document.querySelector('#preloader');
+window.onload = function() {
+    const body = document.querySelector('body');
+    const preloader = document.querySelector('#preloader');
 
-body.classList.remove('load');
-preloader.style.display = 'none';
+    body.classList.remove('load');
+    preloader.style.display = 'none';
 
-const mySlider = document.getElementById('my-slider');
-const sliderValue = document.getElementById('slider-value');
+    const mySlider = document.getElementById('my-slider');
+    const sliderValue = document.getElementById('slider-value');
 
-function slider() {
-    sliderValue.textContent = mySlider.value;
-}
+    function slider() {
+        sliderValue.textContent = mySlider.value;
+    }
 
-slider();
+    slider();
 
-const select = document.querySelector('.select');
-const selectView = document.querySelector('.select__view span');
-const selectInput = document.querySelector('.select__view input');
-const options = document.querySelectorAll('.select__option');
+    const select = document.querySelector('.select');
+    const selectView = document.querySelector('.select__view span');
+    const selectInput = document.querySelector('.select__view input');
+    const options = document.querySelectorAll('.select__option');
 
-select.addEventListener('click', (e) => {
-    e.stopPropagation();
-    select.classList.toggle('select-active');
-});
-
-options.forEach(option => {
-    option.addEventListener('click', () => {
-        selectView.innerText = option.innerText;
-        selectInput.value = option.innerText;
+    select.addEventListener('click', (e) => {
+        e.stopPropagation();
+        select.classList.toggle('select-active');
     });
-});
 
-body.addEventListener('click', () => {
-    select.classList.remove('select-active');
-});
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selectView.innerText = option.innerText;
+            selectInput.value = option.innerText;
+        });
+    });
+
+    body.addEventListener('click', () => {
+        select.classList.remove('select-active');
+    });
+}
